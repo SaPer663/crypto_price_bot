5c4b3ff8a47c9c8d51c07cad83d37dfd83df9801
saper663_home <saper663ru@gmail.com>
2020-11-12 19:47:08 +0500

    changed the sent text

--- api_db.py
+++ api_db.py
@@ -22,11 +22,11 @@ def write_file(write_object):
 def convert_json_to_daily_message(data_json, currency_name):
     currency_data = data_json
     message = f'{currency_name}:\n цена: {currency_data["data"]["last"]}\n\
-         max: {currency_data["data"]["high"]}\n min: {currency_data["data"]["low"]}'
+    max: {currency_data["data"]["high"]}\n min: {currency_data["data"]["low"]}'
     return message
 
 def convert_json_to_warning_message(current_price_value, last_price_value, currency_name):
     message = f'{currency_name}:\n большая волотильность\n\
-         цена изменилась на {last_price_value - current_price_value}\n\
+         цена изменилась на {(last_price_value - current_price_value):.1f}\n\
              текущая цена {current_price_value}'
     return message         
